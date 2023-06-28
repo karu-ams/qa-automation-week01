17dbfc134bab7ba8ff7d2a902c848c0ccdc89895
Karen <karen.soto@radiumrocket.com>
2023-06-28 17:24:52 -0300

    QA-W01: e2e spec for user created

--- test/specs/user.e2e.ts
+++ test/specs/user.e2e.ts
@@ -1,8 +1,28 @@
-import axios from 'axios'
+import { createUser, getUser, deleteUser } from "../utils/apiCommands";
 
-describe('My Login application', () => {
-    it('should login with valid credentials', async () => {
+describe('API commands', () => {
+    let userId: number;
+    let userEmail: string;
+
+    afterAll(async () => {
+        await deleteUser(userId);
     });
+
+    it('POST command', async () => {
+        const user = {
+            name: 'Test UserName',
+            email: 'automation.qa@testing.com'
+        };
+        await createUser(user);
+        userId = 1;
+    });
+
+    it('GET command', async () => {
+        const userData = await getUser(userId);
+        userEmail = userData.email;
+
+        console.log('User email:', await userEmail);
+    })
 });
 
 
